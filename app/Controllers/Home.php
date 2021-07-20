038a8699eec49832dd3ceb1d0c7f2c17ffd697a2
Leonel365 <leonel>
2021-07-20 12:19:48 -0500

    solicion pagina principal

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,16 @@
 
 namespace App\Controllers;
 
+use App\Models\Libro;
 class Home extends BaseController
 {
 	public function index()
 	{
-		return view('libros/crear');
+		$libro= new Libro();
+        $datos['libros']= $libro->orderBy('id','ASC')->findAll();
+
+        $datos['cabecera']= view('template/cabecera');
+        $datos['pie']= view('template/piepagina');
+		return view('libros/crear', $datos);
 	}
 }
--- app/Views/libros/crear.php
+++ app/Views/libros/crear.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="widtd=device-widtd, initial-scale=1.0">
-    <title>Document</title>
-   
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-</head>
-<body>
-Sistema de libros 
-    <div class="container">
+<?=$cabecera?>
 
 Formulario de crear
 
@@ -27,7 +15,7 @@ Formulario de crear
     </div>
 
     <div class="form-group">
-        <label for="imagen">Text</label>
+        <label for="imagen">Seleccione una imagen:</label>
         <input id="imagen" class="form-control" name="imagen" type="file">
     </div>
     <br>
